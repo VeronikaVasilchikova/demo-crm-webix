8da197763265f291ad7fffd1118763206aff6e17
Veronika <v.vasilchykava@gmail.com>
2020-04-17 12:09:20 +0300

    Add dummy data to the models folder

---
+++ sources/models/notes.js
@@ -0,0 +1,12 @@
+export const notes = new webix.DataCollection({
+	data: [
+		{id: 1, clientNameId: "1", value: ""},
+		{id: 2, clientNameId: "2", value: ""},
+		{id: 3, clientNameId: "3", value: ""},
+		{id: 4, clientNameId: "4", value: ""},
+		{id: 5, clientNameId: "5", value: ""},
+		{id: 6, clientNameId: "6", value: ""},
+		{id: 7, clientNameId: "7", value: ""},
+		{id: 8, clientNameId: "8", value: ""}
+	]
+});
---
+++ sources/models/tags.js
@@ -0,0 +1,10 @@
+export const tags = new webix.DataCollection({
+	data: [
+		{id: "1", value: "Furnished", color: "#8664C6"},
+		{id: "2", value: "Bathroom", color: "#BCAAE0"},
+		{id: "3", value: "Balcony", color: "#1CA1C1"},
+		{id: "4", value: "Metro", color: "#5ABBD2"},
+		{id: "5", value: "Shop", color: "#FDBF4C"},
+		{id: "6", value: "School", color: "#F8643F"}
+	]
+});
