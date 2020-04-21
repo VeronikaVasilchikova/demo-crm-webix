9ba3df36fdc3eca9ccb59892247481d3d5e943ea
Veronika <v.vasilchykava@gmail.com>
2020-04-21 19:34:32 +0300

    Create and refresh dummy data

--- sources/models/documents.js
+++ sources/models/documents.js
@@ -12,7 +12,27 @@ export const documents = new webix.DataCollection({
 		{id: 7, dealId: "7", name: "Agreement7", changeDate: "2020-01-18", sizetext: "90"},
 		{id: 8, dealId: "8", name: "Agreement8", changeDate: "2020-04-02", sizetext: "92"},
 		{id: 9, dealId: "9", name: "Agreement9", changeDate: "2020-01-28", sizetext: "110"},
-		{id: 10, dealId: "10", name: "Agreement10", changeDate: "2020-04-02", sizetext: "134"}
+		{id: 10, dealId: "10", name: "Agreement10", changeDate: "2020-04-02", sizetext: "134"},
+		{id: 11, dealId: "11", name: "Agreement11", changeDate: "2020-04-02", sizetext: "84"},
+		{id: 12, dealId: "12", name: "Agreement12", changeDate: "2020-03-02", sizetext: "100"},
+		{id: 13, dealId: "13", name: "Agreement13", changeDate: "2020-04-12", sizetext: "142"},
+		{id: 14, dealId: "14", name: "Agreement14", changeDate: "2020-03-14", sizetext: "96"},
+		{id: 15, dealId: "15", name: "Agreement15", changeDate: "2020-04-02", sizetext: "98"},
+		{id: 16, dealId: "16", name: "Agreement16", changeDate: "2020-04-02", sizetext: "89"},
+		{id: 17, dealId: "17", name: "Agreement17", changeDate: "2020-01-18", sizetext: "90"},
+		{id: 18, dealId: "18", name: "Agreement18", changeDate: "2020-04-02", sizetext: "92"},
+		{id: 19, dealId: "19", name: "Agreement19", changeDate: "2020-01-28", sizetext: "110"},
+		{id: 20, dealId: "20", name: "Agreement20", changeDate: "2020-04-02", sizetext: "134"},
+		{id: 21, dealId: "21", name: "Agreement21", changeDate: "2020-04-02", sizetext: "84"},
+		{id: 22, dealId: "22", name: "Agreement22", changeDate: "2020-03-02", sizetext: "100"},
+		{id: 23, dealId: "23", name: "Agreement23", changeDate: "2020-04-12", sizetext: "142"},
+		{id: 24, dealId: "24", name: "Agreement24", changeDate: "2020-03-14", sizetext: "96"},
+		{id: 25, dealId: "25", name: "Agreement25", changeDate: "2020-04-02", sizetext: "98"},
+		{id: 26, dealId: "26", name: "Agreement26", changeDate: "2020-04-02", sizetext: "89"},
+		{id: 27, dealId: "27", name: "Agreement27", changeDate: "2020-01-18", sizetext: "90"},
+		{id: 28, dealId: "28", name: "Agreement28", changeDate: "2020-04-02", sizetext: "92"},
+		{id: 29, dealId: "29", name: "Agreement29", changeDate: "2020-01-28", sizetext: "110"},
+		{id: 30, dealId: "30", name: "Agreement30", changeDate: "2020-04-02", sizetext: "134"}
 	],
 	scheme: {
 		$init: (obj) => {
@@ -23,4 +43,3 @@ export const documents = new webix.DataCollection({
 		}
 	}
 });
-
--- sources/models/kanbanData.js
+++ sources/models/kanbanData.js
@@ -1,14 +1,98 @@
 export const kanbanData = [
-	{id: 1, status: "new", text: "Text", tags: [1, 2, 3], colors: 1},
-	{id: 2, status: "work", text: "Text", tags: [1], colors: 3},
-	{id: 3, status: "work", text: "Text", tags: [2], colors: 2},
-	{id: 4, status: "work", text: "Text", tags: [3], colors: 2},
-	{id: 5, status: "test", text: "Text", tags: [1, 2], colors: 3},
-	{id: 6, status: "new", text: "Text", tags: [2, 3], colors: 1},
-	{id: 7, status: "test", text: "Text", tags: [1, 2, 3], colors: 2},
-	{id: 8, status: "work", text: "Text", tags: [2], colors: 3},
-	{id: 9, status: "work", text: "Text", tags: [2], colors: 1},
-	{id: 10, status: "work", text: "Text", tags: [3], colors: 1},
-	{id: 11, status: "test", text: "Text", tags: [1, 2], colors: 2},
-	{id: 12, status: "done", text: "Text", tags: [3], colors: 3}
+	{
+		id: 1,
+		user_id: 1,
+		tags: [2],
+		text: "Cyril Barone",
+		color: 1,
+		status: "request"
+	},
+	{
+		id: 2,
+		user_id: 7,
+		tags: [3],
+		text: "Tommie Shurtleff",
+		color: 2,
+		status: "purchase offer"
+	},
+	{
+		id: 3,
+		user_id: 1,
+		tags: [1],
+		text: "Lacy Fordham",
+		color: 3,
+		status: "contacting"
+	},
+	{
+		id: 4,
+		user_id: 6,
+		tags: [3],
+		text: "Abdul Abshire",
+		color: 1,
+		status: "paused"
+	},
+	{
+		id: 5,
+		user_id: 3,
+		tags: [2],
+		text: "Cyril Barone",
+		color: 2,
+		status: "tour scheduled"
+	},
+	{
+		id: 6,
+		user_id: 9,
+		tags: [3],
+		text: "Tommie Shurtleff",
+		color: 3,
+		status: "Escrow"
+	},
+	{
+		id: 7,
+		user_id: 3,
+		tags: [1],
+		text: "Lacy Fordham",
+		color: 1,
+		status: "request"
+	},
+	{
+		id: 8,
+		user_id: 4,
+		tags: [3],
+		text: "Abdul Abshire",
+		color: 1,
+		status: "paused"
+	},
+	{
+		id: 9,
+		user_id: 1,
+		tags: [2],
+		text: "Cyril Barone",
+		color: 2,
+		status: "contacting"
+	},
+	{
+		id: 10,
+		user_id: 7,
+		tags: [3],
+		text: "Tommie Shurtleff",
+		color: 1,
+		status: "listing agreement"
+	},
+	{
+		id: 11,
+		user_id: 1,
+		tags: [1],
+		text: "Lacy Fordham",
+		color: 3,
+		status: "lending"
+	},
+	{
+		id: 12,
+		user_id: 6,
+		tags: [3],
+		text: "Abdul Abshire",
+		color: 3,
+		status: "closing"
+	}
 ];
